4303bde96374cc6efda2d99b5fa29d0ce70868c7
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-05-24 13:03:50 +0900

    @ManyToMany 양방향 및 조회 테스트

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -23,4 +23,9 @@ public class Food {
             joinColumns = @JoinColumn(name = "food_id"), // 현재 위치인 Food Entity 에서 중간 테이블로 조인할 컬럼 설정
             inverseJoinColumns = @JoinColumn(name = "user_id")) // 반대 위치인 User Entity 에서 중간 테이블로 조인할 컬럼 설정
     private List<User> userList = new ArrayList<>();
+
+    public void addUserList(User user) {
+        this.userList.add(user); // 외래 키(연관 관계) 설정
+        user.getFoodList().add(this); //객체지향을 활용하기 위해 외래키의 주인은 아니지만, user엔티티에도 food정보를 넣어놓기
+    }
 }
